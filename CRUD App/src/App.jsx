b1c8ff279fb85ef5ba033fc01160eabdb7026d6a
KartikKonje30 <kartikkonje.01@gmail.com>
2025-06-14 22:59:33 +0530

    Table list UI completed

--- CRUD App/src/App.jsx
+++ CRUD App/src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar"
+import TableList from "./components/TableList"
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className="font-medium text-[5vw] text-slate-500 dark:text-slate-200">
         <Navbar />
+        <TableList />
       </div>
     </>
   )
