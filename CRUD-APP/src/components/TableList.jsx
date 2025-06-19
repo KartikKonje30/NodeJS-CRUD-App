37e5175b60af085266f0462ca3b423c7ace1aae6
KartikKonje30 <kartikkonje.01@gmail.com>
2025-06-19 16:30:56 +0530

    Backend updated with get, post, put and delete funtion

--- CRUD-APP/src/components/TableList.jsx
+++ CRUD-APP/src/components/TableList.jsx
@@ -1,9 +1,17 @@
-
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import axios from 'axios';
+import { useEffect, useState } from "react";
 
 export default function TableList({ handleOpen }) {
-  
+
+  const [tableData, setTableSet] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(()=>{
+    
+  })
+
   let clients = [
     {
       id: 1,
@@ -49,32 +57,39 @@ export default function TableList({ handleOpen }) {
           <tbody>
             {clients.map((client) => {
               return (
-                  <tr key={client.id} className="hover:bg-base-300">
-                    <th>{client.id}</th>
-                    <td>{client.name}</td>
-                    <td>{client.email}</td>
-                    <td>{client.job}</td>
-                    <td>{client.rate}</td>
-                    <td>
-                    <button className={` btn btn-rounded rounded-full w-20 ${client.isactive ? `btn-primary` : `btn-outline btn-primary`}`}>
+                <tr key={client.id} className="hover:bg-base-300">
+                  <th>{client.id}</th>
+                  <td>{client.name}</td>
+                  <td>{client.email}</td>
+                  <td>{client.job}</td>
+                  <td>{client.rate}</td>
+                  <td>
+                    <button
+                      className={` btn btn-rounded rounded-full w-20 ${
+                        client.isactive
+                          ? `btn-primary`
+                          : `btn-outline btn-primary`
+                      }`}
+                    >
                       {client.isactive ? "Active" : "Inactive"}
                     </button>
-                    </td>
-                    <td>
-                    <button 
-                    onClick={() => handleOpen("edit")}
-                    className="btn btn-secondary w-28 flex gap-2" >
-                      Update 
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleOpen("edit")}
+                      className="btn btn-secondary w-28 flex gap-2"
+                    >
+                      Update
                       <FaEdit className="w-4 h-4" />
                     </button>
-                    </td>
-                    <td>
+                  </td>
+                  <td>
                     <button className="btn btn-outline btn-error w-28 flex gap-2">
                       Delete
                       <MdDelete className="w-4 h-4" />
                     </button>
-                    </td>
-                  </tr>
+                  </td>
+                </tr>
               );
             })}
           </tbody>
